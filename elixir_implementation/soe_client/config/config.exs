aafcf8f45629018f10e3be1136dc9bb31035f932
snate <sebastiano.valle@studenti.unipd.it>
2017-01-04 19:08:06 +0100

    Make configs depend on environment for SoE client

--- elixir_implementation/soe_client/config/config.exs
+++ elixir_implementation/soe_client/config/config.exs
@@ -1,4 +1,3 @@
 use Mix.Config
 
-config :soe_client, backend_address: {:Receiver, :"SoE0@127.0.0.1"}
-config :soe_client, endpoint_address: {:Server, :"endpoint@127.0.0.1"}
+import_config "#{Mix.env}.exs"
---
+++ elixir_implementation/soe_client/config/test.exs
@@ -0,0 +1,4 @@
+use Mix.Config
+
+config :soe_client, backend_address: :ReceiverMock
+config :soe_client, endpoint_address: :ServerMock
