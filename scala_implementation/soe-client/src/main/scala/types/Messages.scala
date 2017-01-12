42491784549626bba5e58eb06076f970ecebbd2a
snate <sebastiano.valle@studenti.unipd.it>
2017-01-12 19:33:37 +0100

    [Scala] Fix client

--- scala_implementation/soe-client/src/main/scala/types/Messages.scala
+++ scala_implementation/soe-client/src/main/scala/types/Messages.scala
@@ -4,6 +4,7 @@ object Messages  {
   // backend interface
   case class CheckPrimality(num : Integer)
   // endpoint interface
+  case object Registered
   case class AskFor(number : Integer)
   case class AnswerFor(number : Integer, isPrime : Boolean)
 }
